164a5e2a37932ff868ce0845c7e0f65930415f64
Justin S <justinsr20@gmail.com>
2017-12-19 14:13:38 +1000

    working on geting basic ansible repo / app structure in place

--- create_ec2.sh
+++ create_ec2.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 ansible-playbook -i /etc/ansible/hosts/ec2.py inventory/create_ec2_centos.yml -vvvv
+
+# list hosts
+# ansible-playbook -i /etc/ansible/hosts/ec2.py -u centos inventory/list_ec2s.yml -vvvv

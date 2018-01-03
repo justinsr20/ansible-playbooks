c25fec18450c058c48ccfd68a2c2fd998b50ebfc
Justin S <justinsr20@gmail.com>
2018-01-03 11:38:13 +1000

    run playbook on filtered group of hosts

--- create_ec2.sh
+++ create_ec2.sh
@@ -2,5 +2,5 @@
 
 ansible-playbook -i /etc/ansible/hosts/ec2.py inventory/create_ec2_centos.yml -vvvv
 
-# list hosts
-# ansible-playbook -i /etc/ansible/hosts/ec2.py -u centos inventory/list_ec2s.yml -vvvv
+# list hosts - filter by changing hosts var
+# ansible-playbook -i /etc/ansible/hosts/ec2.py -u centos inventory/list_hosts.yml --extra-vars '{ "hosts" : "all" }'
